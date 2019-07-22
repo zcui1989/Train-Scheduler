2875a9c0b183569291b83071fd0eb029836115ba
Zhen Cui <zcui1989@gmail.com>
2019-07-21 22:49:29 -0400

    database

--- assets/javascript/t.js
+++ assets/javascript/t.js
@@ -1,34 +1,54 @@
-
-// Your web app's Firebase configuration
 var firebaseConfig = {
     //Put your creds here
+    apiKey: "AIzaSyAs48uthGM74LqCjpOZXJydOGd0l-oUNYI",
+    authDomain: "train-schedule-32a08.firebaseapp.com",
+    databaseURL: "https://train-schedule-32a08.firebaseio.com",
+    projectId: "train-schedule-32a08",
+    storageBucket: "",
+    messagingSenderId: "329129705019",
+    appId: "1:329129705019:web:5e04810580245fdf"
 };
-​
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 ​
 // Create a variable to reference the database.
 var database = firebase.database();
+trainName ="",
+destination = "",
+frequency = 0,
+nextArrival = 0,
+minutessAaway = 0,
+trainData = [];
+
 ​
 $('#submitBtn').on("click", function(e) {
     e.preventDefault();
     var input = $('input');
+
+    trainName = $("#train-name").val().trim();
+    destination = $("#destination").val().trim();
+    trainTime = $("#train-time").val().trim();
+    frequency = $("#frequency").val().trim();
 ​
     // Code for handling the push
     database.ref().push({
-        description: input[0].value,
-        time: Date.now()
-    });
+        TRAIN_NAME : trainName, 
+        DESTINATION : destination, 
+        TRAIN_TIME : trainTime,
+        FREQUENCY : frequency
+      });
 ​
     input.val('');
 });
 ​
 database.ref().on("child_added", function(snapshot) {
 // storing the snapshot.val() in a variable for convenience
+
+console.log(snapshot.val());
+
 var data = snapshot.val();
-​
-console.log(data);
-​
+​​
 var now = moment();
 var date = moment(data.time);
 ​
